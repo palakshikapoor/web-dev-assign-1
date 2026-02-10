5c73faad5efd6c0ae20606fb5f6c4a84a94da6fd
Palakshi Kapoor <palakshikapoor03@gmail.com>
2026-02-10 17:43:09 +0530

    Delete todo app directory

--- todo app/todoscript.js
+++
@@ -1,9 +0,0 @@
-let inputBox = document.querySelector("#inputBox")
-let addBtn=document.querySelector("#addBtn")
-let list=document.querySelector('#list')
-
-addBtn.addEventListener("click",function(){
-    let eachList = document.createElement('div')
-    eachList.setAttribute('class','eachlist')
-    eachList.innerHTML
-})
